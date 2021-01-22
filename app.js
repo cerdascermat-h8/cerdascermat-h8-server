87934b65ae27c51f5ffc79759ae12a29e1bc831c
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-22 11:11:49 +0700

    fix: reinstall socket and add cors

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 const port = process.env.PORT || 3000
+const cors = require('cors')
+
+app.use(cors())
 
 const messages = [{
   name : 'welcome',
